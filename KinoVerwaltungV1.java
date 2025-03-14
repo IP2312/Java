2ac5e84a548edab726b603e4fc8cf774322c6f60
Iason <iason.pifeas@gmail.com>
2025-03-14 18:17:09 +0100

    home 14.3

--- KinoVerwaltungV1.java
+++ KinoVerwaltungV1.java
@@ -8,7 +8,6 @@ public class KinoVerwaltungV1 {
         double money = 0;
         ArrayList moviesWatched = new ArrayList();
         String[][] movies = {
-                {"Titel", "Uhrzeit", "Saal", "Restplätze"},
                 {"Heldin", "20:15", "2", "10"},
                 {"A Real Pain", "20:15", "2", "12"},
                 {"Anora", "20:15", "2", "0"},
@@ -17,29 +16,23 @@ public class KinoVerwaltungV1 {
 
         Scanner sc = new Scanner(System.in);
         //ask how much money
-        boolean valid = false;
         do {
             System.out.println("Wie viel Geld hast du bei dir?");
-            try {
-                money = Double.parseDouble(sc.next());
-                if (money > 0) {
-                    valid = true;
-                } else {
-                    System.out.println("Betrag muss mehr als Null sein");
-                }
-            } catch (NumberFormatException ignore) {
-                System.out.println("Ungültiger Input");
+            money = sc.nextDouble();
+            if (money <= 15) {
+                System.out.println("Betrag muss mehr als Null sein");
             }
-        } while (!valid);
+        } while (money <= 15);
 
         //display monitor
         do {
-            String formatter_title = "%-2s %-14s %-10s %-8s %6s\n";
-            System.out.printf(formatter_title, "", movies[0][0], movies[0][1], movies[0][2], movies[0][3]);
-            System.out.println("------------------------------------------------");
+            String formatter_title = "%8s %17s %6s %15s\n";
+            System.out.printf(formatter_title, "Title", "Uhrzeit", "Saal", "Restplätze");
+            System.out.println("----------------------------------------------------");
             //print monitor displaying movie title, time, movie theatre, availability
             String formatter_content = "%d. %-15s %-10s %-8s %6s\n";
-            for (int i = 1; i < movies.length; i++) {
+
+            for (int i = 0; i < movies.length; i++) {
                 int seat = Integer.parseInt(movies[i][3]);
                 String availability;
                 if (seat > 0) {
@@ -47,54 +40,47 @@ public class KinoVerwaltungV1 {
                 } else {
                     availability = "ausgebucht";
                 }
-                System.out.printf(formatter_content, i, movies[i][0], movies[i][1], movies[i][2], availability);
+                System.out.printf(formatter_content, i + 1, movies[i][0], movies[i][1], movies[i][2], availability);
             }
-            System.out.println("------------------------------------------------");
+            System.out.println("-----------------------------------------------------");
 
             //ask which movie until available movie selected or quit
 
             System.out.println("Welchen Film möchtest du sehen?");
-            try {
-                movie = Integer.parseInt(sc.next());
-                if (movie > 0 && movie < movies.length) {
-                    seats = Integer.parseInt(movies[movie][3]);
-                    if (seats != 0) {
-                        // if  available movie selected ask for number of tickets
-                        System.out.println(String.format("Es sind noch %d tickets zu je 15€ verfügbar. Wie viele möchtest du kaufen?", seats));
-                        try {
-                            int gekaufteTickets = Integer.parseInt(sc.next());
-                            if (gekaufteTickets <= 0) {
-                                System.out.println("Anzahl der Tickets darf nicht 0 oder kleiner sein.");
-                            } else if (gekaufteTickets > seats) {
-                                System.out.println("Es sind nicht genügend Plätze vorhanden");
-                            } else {
-                                int preis = gekaufteTickets * 15;
-                                if (preis <= money) {
-                                    money -= preis;
-                                    System.out.println(String.format("Du hast %d Tickets  um %d€ gekauft und hast jetzt noch %.2f€", gekaufteTickets, preis, money));
-                                    // if has enough money and seats are available subtract money and available seats from movie
-                                    int remainingSeats = seats - gekaufteTickets;
-                                    movies[movie][3] = String.valueOf(remainingSeats);
+            movie = sc.nextInt();
+            if (movie > 0 && movie < movies.length) {
+                seats = Integer.parseInt(movies[movie - 1][3]);
+                if (seats > 0) {
+                    // if  available movie selected ask for number of tickets
+                    System.out.println(String.format("Es sind noch %d tickets zu je 15€ verfügbar. Wie viele möchtest du kaufen?", seats));
+                        int gekaufteTickets = sc.nextInt();
+                        if (gekaufteTickets <= 0) {
+                            System.out.println("Anzahl der Tickets darf nicht 0 oder kleiner sein.");
+                        } else if (gekaufteTickets > seats) {
+                            System.out.println("Es sind nicht genügend Plätze vorhanden");
+                        } else {
+                            int preis = gekaufteTickets * 15;
+                            if (preis <= money) {
+                                money -= preis;
+                                System.out.println(String.format("Du hast %d Tickets  um %d€ gekauft und hast jetzt noch %.2f€", gekaufteTickets, preis, money));
+                                // if has enough money and seats are available subtract money and available seats from movie
+                                int remainingSeats = seats - gekaufteTickets;
+                                movies[movie - 1][3] = String.valueOf(remainingSeats);
+                                for (int i = 0; i < gekaufteTickets; i++){
                                     moviesWatched.add(movie);
-                                } else {
-                                    System.out.println("Du hast nicht genug Geld.");
                                 }
+                            } else {
+                                System.out.println("Du hast nicht genug Geld.");
                             }
-                        }catch (NumberFormatException ignore) {
-                            System.out.println("Ungültiger Input");
                         }
-                    } else {
-                        System.out.println("Film ausgebucht");
-                    }
                 } else {
-                    System.out.println("Wähle einen der angezeigten Filme.");
+                    System.out.println("Film ausgebucht");
                 }
-            } catch (NumberFormatException ignore) {
-                System.out.println("Input ungültig");
-                movie = 1;
+            } else {
+                System.out.println("Wähle einen der angezeigten Filme.");
             }
 
-        } while (movie != 0);
+    } while(movie > 0);
         System.out.println("Filme: " + moviesWatched);
-    }
+}
 }
