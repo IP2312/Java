cef17aa0c06551c54eeea6178bf3a9b399c9957b
Iason <iason.pifeas@gmail.com>
2025-03-18 11:12:47 +0100

    18.3.2025 test

--- CyberSecurityV1.java
+++ CyberSecurityV1.java
@@ -5,21 +5,44 @@ import java.util.*;
 
 public class CyberSecurityV1 {
     public static void main(String[] args) {
-        //int [] possibleCharacters = {'A','B','C','a','b','c',0,1,2,'!'};
         List<Character> possibleCharacters = Arrays.asList('A','B','C','a','b','c','0','1','2','!');
-        System.out.println(possibleCharacters);
 
 
         //create Password
         ArrayList password = new ArrayList<>();
         Random r = new Random();
 
+
         for (int i = 0; i < 4; i++){
             char randomElement = possibleCharacters.get(r.nextInt(possibleCharacters.size()));
-            System.out.println(randomElement);
+            password.add(randomElement);
 
         }
-        System.out.println(password);
+        int nrOfGuesses = 0;
+        System.out.println("Password: " + password);
+        int nrPossibleChar = possibleCharacters.size();
+        for (int i = 0; i < nrPossibleChar; i++) {
+            for (int j = 0; j < nrPossibleChar; j++) {
+                for (int k = 0; k <nrPossibleChar; k++) {
+                    for (int l = 0; l < nrPossibleChar; l++) {
+                        ArrayList solution = new ArrayList();
+                        solution.add(possibleCharacters.get(i));
+                        solution.add(possibleCharacters.get(j));
+                        solution.add(possibleCharacters.get(k));
+                        solution.add(possibleCharacters.get(l));
+                        nrOfGuesses++;
+                        if (solution.equals(password)){
+                            System.out.println("Found it: " + solution + " == " + password);
+                            System.out.println("Took " + nrOfGuesses + " guesses");
+                            i=j=k=l=nrPossibleChar;
+                        }
+                    }
+                    
+                }
+                
+            }
+            
+        }
 
     }
 }
