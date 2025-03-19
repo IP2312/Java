b83448209ad97debd36837a522f458fbe54967ae
Iason <iason.pifeas@gmail.com>
2025-03-19 11:31:05 +0100

    19.3.2025 ZahlenRatenV1

---
+++ ZahlenRatenV1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class ZahlenRatenV1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        int level;
+        do {
+            System.out.println("Welches Level möchtest du spielen? (LV1 LV2 LV3)");
+            level = sc.nextInt();
+        }
+
+    }
+}
