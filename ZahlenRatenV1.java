f7b8642033043a526e6e3fb213dc944cb60aeac2
Iason <iason.pifeas@gmail.com>
2025-03-20 09:37:59 +0100

    20.3.2025 ZahlenRatenV1

--- ZahlenRatenV1.java
+++ ZahlenRatenV1.java
@@ -13,7 +13,7 @@ public class ZahlenRatenV1 {
             do {
                 System.out.println("Welches Level möchtest du spielen? Lv:(1 2 3) 0 zum beenden");
                 level = sc.nextInt();
-            } while (level < 0 || level > 4);
+            } while (level < 0 || level > 3);
 
             char answer;
             switch (level) {
@@ -48,21 +48,23 @@ public class ZahlenRatenV1 {
                     do {
                         int randomNr = r.nextInt(101);
                         System.out.println(randomNr);
-                        int tries = 0;
+                        //int tries = 0;
                         int tip;
 
                         ArrayList<Integer> tips = new ArrayList<>();
                         do {
                             System.out.println("Gib einen Tip ab (0-100).");
                             tip = sc.nextInt();
+                            int dif = Math.abs(randomNr - tip);
+                            
                             if (tip == randomNr) {
                                 System.out.println("Erraten");
                             } else {
-                                if (Math.abs(randomNr - tip) <= 3) {
+                                if (dif <= 3) {
                                     System.out.println("fast da (1-3 daneben)");
-                                } else if (Math.abs(randomNr - tip) <= 10) {
+                                } else if (dif <= 10) {
                                     System.out.println("relativ nahe (4-10 daneben)");
-                                } else if (Math.abs(randomNr - tip) <= 20) {
+                                } else if (dif <= 20) {
                                     System.out.println("Nicht ganz so weit weg (11-20) daneben");
                                 } else {
                                     System.out.println("Weit weg (>20) daneben");
@@ -82,8 +84,8 @@ public class ZahlenRatenV1 {
                         System.out.println(randomNr);
                         int tip;
                         boolean myTurn = r.nextBoolean();
-                        int higherthan = 100;
-                        int lowerthan = 0;
+                        int max = 100;
+                        int min = 0;
                         ArrayList<Integer> tips = new ArrayList<>();
                         do {
                             if (myTurn) {
@@ -92,7 +94,7 @@ public class ZahlenRatenV1 {
                             } else {
                                 //calculate tip of computer
 
-                                tip = lowerthan + (higherthan - lowerthan)/2;
+                                tip = min + (max - min)/2;
                                 System.out.println("Tip des Computers: " + tip);
                             }
                             tips.add(tip);
@@ -106,13 +108,13 @@ public class ZahlenRatenV1 {
                                 }
                             } else if (tip > randomNr) {
                                 System.out.println("Kleiner");
-                                if (higherthan > tip){
-                                    higherthan = tip;
+                                if (max > tip){
+                                    max = tip;
                                 }
                             } else {
                                 System.out.println("Größer");
-                                if (lowerthan < tip){
-                                    lowerthan = tip;
+                                if (min < tip){
+                                    min = tip;
                                 }
                             }
                             System.out.println(tips);
