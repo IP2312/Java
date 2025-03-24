3f5ef871cec64f12d0b76aeaca3e57d674279b02
Iason <iason.pifeas@gmail.com>
2025-03-24 19:01:40 +0100

    home 22.3

--- ZahlenRatenV2.java
+++ ZahlenRatenV2.java
@@ -12,15 +12,13 @@ public class ZahlenRatenV2 {
         boolean myTurn = r.nextBoolean();
         ArrayList<Integer> tips = new ArrayList<>();
 
-
         do {
             int randomNr = r.nextInt(101);
             System.out.println("RandomNr: " + randomNr);
+            possiblevalues.clear();
             for (int i = 0; i < 101; i++) {
                 possiblevalues.add(i);
             }
-
-
 
             int tip;
             do {
@@ -40,41 +38,21 @@ public class ZahlenRatenV2 {
 
                 int dif = Math.abs(randomNr - tip);
                 if (tip == randomNr) {
-                    if (myTurn){
+                    if (myTurn) {
                         System.out.println("Erraten");
-                    }else {
+                    } else {
                         System.out.println("Computer gewinnt.");
                     }
 
                 } else {
-                    if (dif <= 3) {
-                        possiblevalues.remove(Integer.valueOf(tip));
-                        System.out.println("fast da (1-3 daneben)");
-                        remove(1, 3, tip);
-                    } else if (dif <= 10) {
-                        System.out.println("relativ nahe (4-10 daneben)");
-                        remove(4, 10, tip);
-                    } else if (dif <= 20) {
-                        System.out.println("Nicht ganz so weit weg (11-20) daneben");
-                        remove(11, 20, tip);
-                    } else {
-                        System.out.println("Weit weg (>20) daneben");
-                        for (int i = possiblevalues.size() - 1; i >= 0; i--) {
-                            int min = tip - 20;
-                            int max = tip + 20;
-                            if ((possiblevalues.get(i) >= min && possiblevalues.get(i) <= max)) {
-                                possiblevalues.remove(i);
-                            }
-                        }
-                    }
+                    feedback(dif, tip);
                     tips.add(tip);
                     System.out.println("Tips: " + tips);
 
                     for (int i = 0; i <= 100; i++) {
-                        if (possiblevalues.contains(i)){
+                        if (possiblevalues.contains(i)) {
                             System.out.print(i + ",");
-                        }
-                        else {
+                        } else {
                             System.out.print("-");
                         }
                     }
@@ -86,6 +64,23 @@ public class ZahlenRatenV2 {
             answer = sc.next().toUpperCase().charAt(0);
         } while (answer == 'Y');
 
+    }
+
+    static void feedback(int dif, int tip){
+        if (dif <= 3) {
+            possiblevalues.remove(Integer.valueOf(tip));
+            System.out.println("fast da (1-3 daneben)");
+            remove(1, 3, tip);
+        } else if (dif <= 10) {
+            System.out.println("relativ nahe (4-10 daneben)");
+            remove(4, 10, tip);
+        } else if (dif <= 20) {
+            System.out.println("Nicht ganz so weit weg (11-20) daneben");
+            remove(11, 20, tip);
+        } else {
+            System.out.println("Weit weg (>20) daneben");
+            remove(20, 100, tip);
+        }
     }
 
     static void remove(int min, int max, int tip) {
