74803e45c5b3501e6119b8881b755d9e8c5bfd93
Iason <iason.pifeas@gmail.com>
2025-03-25 20:36:29 +0100

    home 24.3

--- RPGV2.java
+++ RPGV2.java
@@ -8,7 +8,7 @@ public class RPGV2 {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
         int lifeMax = 10;
-                int mLife = 0;
+        int mLife = 0;
         int pLife = lifeMax;
         int pGold = 1000;
         boolean gameOver = false;
@@ -26,21 +26,21 @@ public class RPGV2 {
 
                 {"4", "6", "", ""},
 
-                {"6", "7", "Versuche das Monster mit deinem Schwert zu erwischen", "Du wirfst dich in die Schlacht!", "6"},
+                {"6", "7", "Versuche das Monster mit deinem Schwert zu erwischen", "Du wirfst dich in die Schlacht!", "4"},
                 {"6", "8", "Es ist Zeit zu gehen", "Du versuchst zu flüchten, doch das Monster verfolgt dich.", "0"},
 
                 {"-1", "9", "", "Deine Zeit ist leider vorüber. Der Tod erwartet dich schon mit freundlichem Grinsen"}, // TOD
                 {"-1", "10", "", "Du bist der größte Held aller Zeiten! Hurra!"}, // SIEG
         };
 
-        do{
+        do {
             System.out.println(choices[currentChoice][3]);
 
             //find valid choices and display them
             System.out.println("-------------------------------");
-            ArrayList <Integer> validOptions = new ArrayList<>();
+            ArrayList<Integer> validOptions = new ArrayList<>();
             for (int i = 0; i < choices.length; i++) {
-                if (choices[i][0] == choices[currentChoice][1]){
+                if (choices[i][0] == choices[currentChoice][1]) {
                     validOptions.add(i);
                     System.out.println(i + ".) " + choices[i][2]);
                 }
@@ -51,43 +51,48 @@ public class RPGV2 {
 
             switch (currentChoice) {
                 case 2:
-                    if (pGold > 100){
+                    if (pGold > 100) {
                         pGold -= 100;
                         pLife += 3;
                         System.out.println(choices[currentChoice][3]);
                         System.out.printf("Dein Lebensdurst kehrt zurück! +3 Leben, jetzt hast du wieder %d Leben!", pLife);
                         System.out.println();
-                    }
-                    else {
+                    } else {
                         System.out.println("Du hast zu wenig Geld");
                     }
                     currentChoice = Integer.parseInt(choices[currentChoice][4]);
-
+                    break;
                 case 3:
-                    if (pGold >= 1000 && lifeMax >= 15){
+                    if (pGold >= 1000 && lifeMax >= 15) {
                         System.out.println(choices[10][3]);
                         gameOver = true;
-                    }
-                    else {
+                    } else {
                         System.out.println(choices[currentChoice][3]);
                         System.out.println("Die Prinzessin belächelt dich nur verächtlich. So einen armen Schlucker soll sie heiraten? Und so wenig Lebenserfahrung! Tz tz!");
-                        System.out.printf("Du braucht noch sicher %d Gold dafür, Junge! Und %d Jahre Erfahrung!",1000-pGold,15- pLife );
+                        System.out.printf("Du braucht noch sicher %d Gold dafür, Junge! Und %d Jahre Erfahrung!", 1000 - pGold, 15 - pLife);
                         System.out.println();
                         currentChoice = Integer.parseInt(choices[currentChoice][4]);
                     }
-
+                    break;
                 case 4:
                     mLife = r.nextInt(18) + 3;
                     System.out.printf("Das Monster hat insgesammt %d Leben, und du noch %d!", mLife, pLife);
                     currentChoice = Integer.parseInt(choices[currentChoice][4]);
-
+                    break;
+                case 6:
+                    System.out.println("pubs");
+                    break;
                 case 7:
-                    System.out.println("Du wirfst dich in die Schlacht!");
                     int pAttack = r.nextInt(pLife) + 1;
                     int mAttack = r.nextInt(mLife) + 1;
+                    System.out.println("Das monster Pubst");
+                    currentChoice = Integer.parseInt(choices[currentChoice][4]);
+
+                    break;
 
             }
 
-        }while (!gameOver);
+
+        } while (!gameOver);
     }
 }
